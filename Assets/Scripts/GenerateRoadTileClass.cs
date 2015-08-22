63b53b830c8ccfab376dcabf4a744d8b0c1484d0
Avenash Krishnakumar <avenash555@gmail.com>
2015-08-22 22:37:48 +0530

    GameScene created with Infinite path

---
+++ Assets/Scripts/GenerateRoadTileClass.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateRoadTileClass : MonoBehaviour {
+
+	private float distance = 15.0f;
+	public GameObject roadTiles;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("CreateRoad", 0f, 2.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void CreateRoad(){
+		distance += 15.0f;
+
+		Vector3 spawnPosition = new Vector3(0,0, distance);
+		Instantiate (roadTiles, spawnPosition, roadTiles.transform.rotation);
+
+	}
+}
---
+++ Assets/Scripts/RoadTileClass.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoadTileClass : MonoBehaviour {
+
+	float timeLimit = 30.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Update(){
+		//if (Vector3. (Camera.main.transform.position, transform.position) > 100.0f) {
+			//Destroy(gameObject);
+		//}
+
+		float distance = gameObject.transform.position.z + 100 - Camera.main.transform.position.z;
+
+		if (distance <= 0) {
+			Destroy(gameObject);
+		}
+	}
+
+}
