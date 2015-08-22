f7cd4d9c37137e713ae167f70ee408061e8bef47
Avenash Krishnakumar <avenash555@gmail.com>
2015-08-23 02:18:40 +0530

    Movement added to vehicle and camera

--- Assets/Scripts/VehicleClass.cs
+++ Assets/Scripts/VehicleClass.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class VehicleClass : MonoBehaviour {
 
 	int vehicleDirection = -1;
-	float speed = 10.0f;
+	float speed = 5.0f;
 
 	Rigidbody rb;
 
@@ -14,20 +14,25 @@ public class VehicleClass : MonoBehaviour {
 
 	void Update () {
 
-		gameObject.transform.Translate (Vector3.forward);
+		Camera.main.transform.Translate (0, 0, speed * Time.deltaTime, Space.World);
+		gameObject.transform.Translate (0, 0, speed * Time.deltaTime, Space.World);
+
+		Vector3 turnForce = new Vector3 (vehicleDirection * 1000, 0, 0);
+
+		rb.AddForce (turnForce);
 
 		if (Input.GetMouseButtonDown (0)) {
 			vehicleDirection *= -1;
 		}
 
-		Move ();
+		//Move ();
 	}
 
 	void Move() {
 		//transform.Translate (speed * Time.deltaTime * vehicleDirection, 0, 0);
 
-		Vector3 force = new Vector3 (vehicleDirection * 1000, 0, 0);
-		rb.AddForce (force);
+		//Vector3 force = new Vector3 (vehicleDirection * 1000, 0, 0);
+		//rb.AddForce (force);
 	}
 
 }
