44428c7be1d1bcdf5250ac0be0cbd3d249dfd24e
Avenash Krishnakumar <avenash555@gmail.com>
2015-08-23 15:09:20 +0530

    Vehicle container script added

---
+++ Assets/Scripts/VehicleContainerClass.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehicleContainerClass : MonoBehaviour {
+
+	private float speed = 5.0f;
+	bool value = false; 
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Camera.main.transform.Translate (0, 0, speed * Time.deltaTime, Space.World);
+
+		gameObject.transform.Translate (0, 0, speed * Time.deltaTime, Space.World);
+
+
+		if (Input.GetMouseButtonDown(0) && value == false)
+		{
+			value = true;
+		}
+		else if (Input.GetMouseButtonDown(0) && value == true)
+		{
+			value = false;
+		}
+		
+		if (value == false)
+		{
+			transform.Translate(new Vector3(1 * Time.deltaTime * 4, 0, 0));
+
+		}
+		else if (value == true)
+		{
+			transform.Translate(new Vector3(-1 * Time.deltaTime * 4, 0, 0));
+
+		}
+
+	}
+}
